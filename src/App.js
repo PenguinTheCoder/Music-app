e806cbc85fe96951fee5ae79e9237a886741bbc4
VWcoder <antonio.konta.face@gmail.com>
2022-07-12 14:32:17 +0200

    creating of app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
+
+function App() {
+  return (
+    <div>
+    <Routes>
+
+        <Route path="/" element={<Home/>} />
+
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Home.css";
+
+
+const Home = () => {
+return (
+<div>
+<header>
+  <h1>TOP 10</h1>
+</header>
+  <div className="song-container">
+    <div className="container1">
+    <p > Aloooo </p>
+    </div>
+    <hr></hr>
+    <div className="container2">
+    <p> banana </p>
+    </div>
+    </div>
+</div>
+)
+}
+
+export default Home;
